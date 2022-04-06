91812c5202cd48c431483dd219eb5737fb335bce
WaveBreath <1517794108@qq.com>
2022-04-06 16:39:13 +0800

    V1.0

---
+++ ACE-Macro/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using PS4MacroAPI;
+
+namespace ACE_Macro
+{
+    public class Script:ScriptBase
+    {
+        public Script()
+        {
+            Config.Name = "Ace Combat 7 ";
+
+        }
+        public override void Start()
+        {
+            base.Start();
+        }
+        public override void Update()
+        {
+            base.Update();
+            Press(new DualShockState() {Cross=true });
+            Sleep(3*1000);
+            flyHigh();
+            loop();
+            retry();
+        }
+        void retry()
+        {
+            int waitDeley = 10;
+            Press(new DualShockState() { Options = true });
+            Sleep(waitDeley);
+            Press(new DualShockState() { DPad_Down = true });
+            Sleep(waitDeley);
+            Press(new DualShockState() { Cross = true });
+            Sleep(waitDeley);
+            Press(new DualShockState() { DPad_Up = true });
+            Sleep(waitDeley);
+        }
+        void flyHigh()
+        {
+            PlayMacro("preprocess.xml");
+        }
+        void loop()
+        {
+            Press(new DualShockState() { R1=true, R2 = 0xff },29*60*1000);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# playstation 皇牌空战7 76500km脚本。
+利用ps remote play，使用PC控制主机。利用pc主机宏，自动飞行，完成奖杯。
+脚本行为：
+1. 按下X键（进入任务）
+2. 自动飞行30分钟
+3. 重启任务，下接第1步
+# 使用方法：
+1. 设置ps remote play
+   https://remoteplay.dl.playstation.net/remoteplay/lang/cs/index.html
+2. 下载ps4Macro
+   https://github.com/komefai/PS4Macro
+3. 进入自由飞行模式，选择关卡洛卡洛哈（long day）。
+4. 选择机体米格-31，任意SP武器
+5. 插件：加速1、最高速1、最高速+加速
+6. 开始任务
+7. 暂停并选择重启任务，**不要直接重启，只把光标放在YES上**。
+8. 启动PS4Macro，打开bin文件夹下的ACE-Macro.dll，运行
